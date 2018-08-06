e6785b7fe77befd9392759bfa2604a761d17b5a9
Sebastian Pauka <spauka@gmail.com>
2018-08-06 16:23:04 +1000

    Add error handling to midas sweeps

--- simple_midas_exp.py
+++ simple_midas_exp.py
@@ -9,26 +9,20 @@ import qcodes_measurements as qcm
 from qcodes_measurements import pyplot
 from qcodes_measurements.plot import plot_tools
 from scipy import signal
+import math
 
-midas.raster_points(2048)
-midas.sw_mode(0)
+midas.sw_mode('distributed')
+midas.filter_mode('5k')
 midas.raster_rate(8)
-midas.num_sweeps_2d(1)
+midas.num_sweeps_2d(4)
 
-midas.ch1.frequency(370.5e6)
-
-for param in ("magnitude", "phase", "I", "Q"):
-    param = getattr(midas.ch1, param)
-    param.shape=(2048,)
-    param.setpoint_names=("Voltage",)
-    param.setpoints=(np.linspace(0, 1, 2048),)
-    param.setpoint_labels = ("Voltage", )
-    param.setpoint_units = ("V", )
+midas.ch1.frequency(532e6)
 
 def _ensure_list(item):
     if any(isinstance(item, x) for x in (list, tuple)):
         return item
     return [item]
+
 
 
 def linear1d_midas(midas, param_set, start, stop, num_points, delay, *param_meas, 
@@ -40,11 +34,14 @@ def linear1d_midas(midas, param_set, start, stop, num_points, delay, *param_meas
     atstart = _ensure_list(kwargs.pop("atstart", []))
     ateach = _ensure_list(kwargs.pop("ateach", []))
     atend = _ensure_list(kwargs.pop("atend", []))
-    ateach.append(midas.averaged_1d_trace())
+    ateach.append(midas.averaged_1d_trace)
     
-    # Do the sweep
-    res = qcm.linear1d(param_set, start, stop, num_points, delay, *param_meas,
-                       atstart=atstart, ateach=ateach, atend=atend, **kwargs)
+    try:
+        # Do the sweep
+        res = qcm.linear1d(param_set, start, stop, num_points, delay, *param_meas,
+                           atstart=atstart, ateach=ateach, atend=atend, **kwargs)
+    finally:
+        midas.visa_handle.clear()
     
     # Return the result
     return res
