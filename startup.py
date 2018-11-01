d42b28c6e430d548ca5b4cbae030b3801f4c4064
Sebastian Pauka <spauka@gmail.com>
2018-11-01 17:19:48 +1100

    Cleanup startup scripts

--- startup.py
+++ startup.py
@@ -5,13 +5,18 @@ Created on Mon Apr 30 12:21:38 2018
 @author: Administrator
 """
 
+import time
+import numpy as np
+import scipy, scipy.constants
+import matplotlib.pyplot as plt
+
 import qcodes as qc
 from qcodes import ChannelList, Parameter
 from qcodes.dataset.measurements import Measurement
 from qcodes.dataset.plotting import plot_by_id
+from qcodes.dataset.database import initialise_database
 from qcodes.dataset.data_set import load_by_id, load_by_counter
-from qcodes.dataset.experiment_container import new_experiment, load_experiment_by_name
-from qcodes.instrument_drivers.Keysight.N5245A import N5245A
+from qcodes.dataset.experiment_container import load_or_create_experiment
 
 from qdev_wrappers.station_configurator import StationConfigurator
 
@@ -19,19 +24,15 @@ import qcodes_measurements as qcm
 from qcodes_measurements.plot.plot_tools import *
 from qcodes_measurements.device import *
 
-import time
-import numpy as np
-import scipy
-
 from dac_params import *
-
-import matplotlib.pyplot as plt
 
 # Log all output
 from IPython import get_ipython
 ip = get_ipython()
 ip.magic("logstop")
 ip.magic("logstart -o -t iPython_Logs\\fivedot_log.py rotate")
+ip.magic("load_ext autoreload")
+ip.magic("autoreload 0")
 
 # Close any instruments that may already be open
 instruments = list(qc.Instrument._all_instruments.keys())
@@ -42,24 +43,21 @@ for instrument in instruments:
 del instruments
 
 exp_name = 'QDP_FIVEDOT'
-sample_name = 'M02_24_17.1_0003_MARIAH_D2345'
+sample_name = 'DARLINGTON_D1'
 
-try:
-    exp = load_experiment_by_name(exp_name, sample=sample_name)
-    print('Experiment loaded. Last ID no:', exp.last_counter)
-except ValueError:
-    exp = new_experiment(exp_name, sample_name)
-    print('Starting new experiment.')
+initialise_database()
+exp = load_or_create_experiment(exp_name, sample_name)
+print('Experiment loaded. Last ID no:', exp.last_counter)
 
 scfg = StationConfigurator()
 
 lockin = scfg.load_instrument('sr860')
 ithaco = scfg.load_instrument('ithaco')
-qubit_source = scfg.load_instrument('qubit_source')
-lo_source = scfg.load_instrument('lo_source')
-# dso = scfg.load_instrument('dso')
-midas = scfg.load_instrument('midas')
-atten = scfg.load_instrument('atten')
+#qubit_source = scfg.load_instrument('qubit_source')
+#lo_source = scfg.load_instrument('lo_source')
+#dso = scfg.load_instrument('dso')
+#midas = scfg.load_instrument('midas')
+#atten = scfg.load_instrument('atten')
 mdac = scfg.load_instrument('mdac')
 yoko = scfg.load_instrument('yoko')
 dmm = scfg.load_instrument('dmm')
@@ -80,46 +78,71 @@ SHORTS_NUMS = (x-1 for x in tuple())
 SHORTS = qcm.make_channel_list(mdac, "Shorts", SHORTS_NUMS)
 
 # Cold Gate Nums
-OHMICS_NUMS = tuple(x-1 for x in (2, 4, 10, 12, 20, 21, 24, 31, 38, 39, 47))
-GATE_NUMS   = tuple(x-1 for x in (1, 3, 5, 6, 7, 8, 9, 11, 13, 14, 15, 16, 17,
-                                  18, 19, 22, 23, 25, 26, 27, 28, 29, 30, 32,
-                                  33, 34, 35, 36, 37, 40, 41, 42, 43, 44, 45,
-                                  46, 48))
+OHMICS_NUMS = tuple(x-1 for x in (1, 5, 12, 17, 38, 39))
+GATE_NUMS   = tuple(x-1 for x in (13, 28, 2, 41, 27, 3, 14, 37, 42, 24, 25,
+                                  6, 19, 20, 18, 44, 26, 29, 4, 16, 8, 40))
 
 # Device 5 Definition
-dev5 = Device("Device_5")
+dev1 = Device("Device_1")
 
-dev5.add_gate("LW1", bb2.ch03.connect_dac(mdac.ch49))
-dev5.add_gate("LP1", bb1.ch07.connect_dac(mdac.ch50))
-dev5.add_gate("C1",  bb1.ch19.connect_dac(mdac.ch51))
-dev5.add_gate("RP1", bb1.ch04.connect_dac(mdac.ch52))
-dev5.add_gate("RW1", bb1.ch14.connect_dac(mdac.ch53))
-dev5.add_gate("N1",  bb2.ch05.connect_dac(mdac.ch54))
-dev5.add_gate("LSDC",bb1.ch17.connect_dac(mdac.ch55))
+dev1.add_gate("LW1", mdac.ch13)
+dev1.add_gate("LP1", mdac.ch28)
+dev1.add_gate("C1",  mdac.ch02)
+dev1.add_gate("RP1", mdac.ch41)
+dev1.add_gate("RW1", mdac.ch27)
+dev1.add_gate("N1",  mdac.ch04)
+dev1.add_gate("LSDT",mdac.ch16)
+dev1.add_gate("LSDC",mdac.ch08)
+dev1.add_gate("LSDB",mdac.ch40)
 
-dev5.add_gate("LW2", bb2.ch15.connect_dac(mdac.ch57))
-dev5.add_gate("LP2", bb1.ch18.connect_dac(mdac.ch58))
-dev5.add_gate("C2",  bb1.ch05.connect_dac(mdac.ch59))
-dev5.add_gate("RP2", bb1.ch16.connect_dac(mdac.ch60))
-dev5.add_gate("RW2", bb1.ch01.connect_dac(mdac.ch61))
-dev5.add_gate("N2",  mdac.ch44)
-dev5.add_gate("RSDC",mdac.ch08)
+dev1.add_gate("LW2", mdac.ch14)
+dev1.add_gate("LP2", mdac.ch37)
+dev1.add_gate("C2",  mdac.ch42)
+dev1.add_gate("RP2", mdac.ch24)
+dev1.add_gate("RW2", mdac.ch25)
+dev1.add_gate("N2",  mdac.ch18)
+dev1.add_gate("RSDT",mdac.ch20)
+dev1.add_gate("RSDC",mdac.ch19)
+dev1.add_gate("RSDB",mdac.ch06)
 
-dev5.add_ohmic("O1", bb2.ch16)
-dev5.add_ohmic("O2", bb1.ch02)
-dev5.add_ohmic("O3", bb2.ch17)
-dev5.add_ohmic("O4", mdac.ch20)
-dev5.add_ohmic("O5", bb1.ch03)
-dev5.add_ohmic("O6", bb2.ch02)
+dev1.add_gate("JBL",mdac.ch29)
+dev1.add_gate("JBT",mdac.ch26)
+dev1.add_gate("JBR",mdac.ch44)
+dev1.add_gate("JBB",mdac.ch03)
 
-qc.Station.default.add_component(dev5, "Dev5")
-qc.Monitor(*(qc.Monitor.running._parameters + tuple(dev5.parameters.values())[1:]))
+dev1.add_ohmic("O1", mdac.ch12)
+dev1.add_ohmic("O2", mdac.ch39)
+dev1.add_ohmic("O3", mdac.ch17)
+dev1.add_ohmic("O4", mdac.ch05)
+dev1.add_ohmic("O5", mdac.ch38)
+dev1.add_ohmic("O6", mdac.ch01)
+
+qc.Station.default.add_component(dev1, "Dev1")
+
+qc.Monitor(*(qc.Monitor.running._parameters +
+             tuple(dev1.parameters.values())[1:]))
+
+
+def conv_r(current, voltage=50e-6, ithaco_imp=200, filtering=13400):
+    r = voltage/current
+    r -= ithaco_imp
+    r -= filtering
+    return r
+
+
+def conv_g(resistance):
+    cc = scipy.constants.value("conductance quantum")
+    g = 1/(cc*resistance)
+    return g
+
+
 
 # Raster Parameters
-#dso.ch1.trace.prepare_curvedata()
+# dso.ch1.trace.prepare_curvedata()
 
-#raster = RasterParam("Raster_LW2", mdac.ch57.voltage, midas.ch1.I)
-#raster_phase = RasterParam("Raster_LW2_Ph", mdac.ch57.voltage, midas.ch1.phase)
+# raster = RasterParam("Raster_LW2", dev5.LW2.source.dac_source.voltage, dso.ch1.trace)
+# raster_phase = RasterParam("Raster_LW2_Ph", mdac.ch57.voltage, midas.ch1.phase)
 #
-#raster_cut = qcm.CutWrapper(raster, fromstart=40)
-#raster_diff = qcm.DiffFilter(raster_cut)
+# raster_cut = qcm.CutWrapper(raster, fromstart=40, fromend=40)
+# raster_diff = qcm.DiffFilter(raster)
+# raster_diff._unit = "dV<sub>rf</sub>/dV<sub>G</sub>"
--- startup_noeqp.py
+++ startup_noeqp.py
@@ -1,36 +1,38 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed May  9 10:28:38 2018
+Created on Mon Apr 30 12:21:38 2018
 
 @author: Administrator
 """
+
+import time
+import numpy as np
+import scipy, scipy.constants
+import matplotlib.pyplot as plt
+
+import qcodes as qc
+from qcodes import ChannelList, Parameter
+from qcodes.dataset.measurements import Measurement
+from qcodes.dataset.plotting import plot_by_id
+from qcodes.dataset.database import initialise_database
+from qcodes.dataset.data_set import load_by_id, load_by_counter
+from qcodes.dataset.experiment_container import load_or_create_experiment
+
+from qdev_wrappers.station_configurator import StationConfigurator
+
+import qcodes_measurements as qcm
+from qcodes_measurements.plot.plot_tools import *
+from qcodes_measurements.device import *
+
+from dac_params import *
 
 # Log all output
 from IPython import get_ipython
 ip = get_ipython()
 ip.magic("logstop")
 ip.magic("logstart -o -t iPython_Logs\\fivedot_log.py rotate")
-
-import qcodes as qc
-from qcodes import ChannelList, Parameter
-from qcodes.dataset.measurements import Measurement
-from qcodes.dataset.plotting import plot_by_id
-from qcodes.dataset.data_set import load_by_id, load_by_counter
-from qcodes.dataset.experiment_container import new_experiment, load_experiment_by_name
-from qcodes.instrument_drivers.Keysight.N5245A import N5245A
-
-from qdev_wrappers.station_configurator import StationConfigurator
-
-import qcodes_measurements as qcm
-from qcodes_measurements.plot.plot_tools import *
-
-import time
-import numpy as np
-import scipy
-
-from dac_params import *
-
-import matplotlib.pyplot as plt
+ip.magic("load_ext autoreload")
+ip.magic("autoreload 0")
 
 # Close any instruments that may already be open
 instruments = list(qc.Instrument._all_instruments.keys())
@@ -41,11 +43,8 @@ for instrument in instruments:
 del instruments
 
 exp_name = 'QDP_FIVEDOT'
-sample_name = 'M02_24_17.1_0003_MARIAH_D2345'
+sample_name = 'DARLINGTON_D1'
 
-try:
-    exp = load_experiment_by_name(exp_name, sample=sample_name)
-    print('Experiment loaded. Last ID no:', exp.last_counter)
-except ValueError:
-    exp = new_experiment(exp_name, sample_name)
-    print('Starting new experiment.')
+initialise_database()
+exp = load_or_create_experiment(exp_name, sample_name)
+print('Experiment loaded. Last ID no:', exp.last_counter)
